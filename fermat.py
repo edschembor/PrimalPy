3910efcaebae8f29b83e3bd7b5d25cffdfe3b5b4
Edward <eschemb1@jhu.edu>
2014-02-20 18:04:19 -0500

    Added Fermat Primality Test

---
+++ fermat.py
@@ -0,0 +1,18 @@
+#Edward Schembor
+#PrimalPy Package
+#Fermat Primality Test
+
+#Fermat's probabalistic primality test
+#
+
+def fermatTest( testNum, checks ):
+	successCount = 0
+	if( checks <= testNum/10 ):
+		checks = testNum/8
+	for x in range(0, checks):
+		a = random.randint(2, testNum-1)
+		if( (a**(testNum-1))%testNum == 1):
+			successCount++;
+		else:
+			return False
+	return successCount
