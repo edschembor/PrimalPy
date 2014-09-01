b03893a5f8b8e05c935e97fff97b02817a1ed346
Edward <eschemb1@jhu.edu>
2014-09-01 13:42:32 -0400

    Finished getPrimeArray
    I think? Still needs test cases

--- getPrimeArray.py
+++ getPrimeArray.py
@@ -15,27 +15,43 @@ def get_prime_array( high ):
     int array : The primes less than high
     """
 
+    #Array of pre-generated primes less than high
+    primes = []
+
     #Need to cleanse this to avoid repitition
     max_one_hundred_thousand = open("./pre_generated_primes/primes-to-100k.txt")
     one_hundred = max_one_hundred_thousand.readlines()
     max_one_hundred_thousand.close()
+    primes.extend(one_hundred)
 
     if ( high > 100000 ):
         max_two_hundred_thousand = open("./pre_generated_primes/primes-to-200k.txt")
         two_hundred = max_two_hundred_thousand.readlines()
         max_two_hundred_thousand.close()
+        primes.extend(two_hundred)
 
     if ( high > 200000 ):
         max_three_hundred_thousand = open("./pre_generated_primes/primes-to-300k.txt")
         three_hundred = max_three_hundred_thousand.readlines()
         max_three_hundred_thousand.close()
+        primes.extend(three_hundred)
 
     if ( high > 300000 ):
         max_four_hundred_thousand = open("./pre_generated_primes/primes-to-400k.txt")
         four_hundred = max_four_hundred_thousand.readlines()
         max_four_hundred_thousand.close()
+        primes.extend(four_hundred)
 
-    if ( high > 500000 ):
+    if ( high > 400000 ):
         max_five_hundred_thousand = open("./pre_generated_primes/primes-to-500k.txt")
         five_hundred = max_five_hundred_thousand.readlines()
         max_five_hundred_thousand.close()
+        primes.extend(five_hundred)
+		
+    for x in range(0, len(primes)-1).reverse():
+        if primes[x] > high:
+            primes.pop(x)
+        else:
+            break
+
+    return primes
