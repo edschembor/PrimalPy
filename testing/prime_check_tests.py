715c5deb17d66ae2c8505571c6bcac2f52223f44
Edward <eschemb1@jhu.edu>
2014-09-04 18:27:09 -0400

    Moved tests and included get_prime_array test

--- testing/prime_check_tests.py
+++ testing/prime_check_tests.py
@@ -4,11 +4,23 @@ import unittest
 from getPrimeArray.py import get_prime_array
 from pocklington_lehmer.py import pocklington_lehmer
 
+class TestGetPrimeArray(unittest.TestCase):
+
+    def setUp(self):
+        #Create a prime array using the get_prime_array method
+        self.primeSet = get_prime_array( 250000 )
+    
+    def testCorrect(self):
+        #Make sure general properties are correct
+        self.assertEqual( len(self.primeSet), 22044 ) #Correct number from WolframAlpha
+        self.assertEqual( self.primeSet[22043], 249989 )
+        self.assertEqual( self.primeSet[0], 2 )
+
 class TestPocklingtonLehmer(unittest.TestCase):
 
     def setUp(self):
-        #Use the pre-generated primes to test the primality tests - we test with all primes <= 500000
-        self.primeSet = get_prime_array(500000)
+        #Use the pre-generated primes to test the primality tests - we test with all primes <= 200000
+        self.primeSet = get_prime_array(200000)
         self.notPrimes = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20]
 
     def test_true(self):
@@ -19,5 +31,6 @@ class TestPocklingtonLehmer(unittest.TestCase):
             # Makes sure the test returns false for non-primes
             assertFalse( pocklington_lehmer( notPrime ) )
 
+			
 if __name__ == '__main__':
     unittest.main()
